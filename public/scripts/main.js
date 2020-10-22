04c84a02c58f51f16a405860a24d4ca1a76dd0d4
Luke Ferderer <ferderl1@rose-hulman.edu>
2020-10-22 18:21:59 -0400

    Unbroke javascript

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,15 +11,15 @@ var rhit = rhit || {};
 rhit.navBarTemplate;
 
 rhit.main = function () {
-	if(document.querySelector("#mainPage")) {
-		rhit.navBarTemplate = document.querySelector("#navBarTemplate");
-	}
-	// TODO: Add in once hosted
+	// TODO: NavBar
+	// if(document.querySelector("#mainPage")) {
+	// 	rhit.navBarTemplate = document.querySelector("#navBarTemplate");
+	// }
 	// if(!rhit.navBarTemplate) {
 	// 	window.location.href = "/";
 	// }
-	const templateClone = rhit.navBarTemplate.content.cloneNode(true).firstElementChild;
-	document.querySelector("body").appendChild(templateClone);
+	// const templateClone = rhit.navBarTemplate.content.cloneNode(true).firstElementChild;
+	// document.querySelector("body").appendChild(templateClone);
 };
 
 rhit.main();

9824b0b4b96ec523276d94eedd39ddb74e71233d
Griffin Annis <annisg@rose-hulman.edu>
2020-10-29 12:46:40 -0400

    quick and dirty cave sizes/locations

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -15,16 +15,34 @@ rhit.CaveSystemGenerator = class {
 	constructor() {};
 
 	generateSystem(numCaves, generateEntranceExit = true) {
-		let cavesToGenerate = numCaves ? numCaves : Math.floor(Math.random() * 7) + 2;
-		console.log(cavesToGenerate);
+		let cavesToGenerate = numCaves ? numCaves : rhit.randomRange(3, 8);
+		let caveSystem = [];
+
+		for(let i = 0; i < cavesToGenerate; i++) {
+			caveSystem[i] = new rhit.Cave();
+		}
+
+		return caveSystem;
 	};
+}
+
+rhit.Cave = class {
+	constructor() {
+		this.size = rhit.randomRange(1, 6);
+		this.x = rhit.randomRange(0, 100);
+		this.y = rhit.randomRange(0, 100);
+	}
+}
+
+rhit.randomRange = function(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 
 rhit.main = function () {
 	$("#navBar").load("/templates.html #navBar");
 	rhit.caveSystemGenerator = new rhit.CaveSystemGenerator();
-	rhit.caveSystemGenerator.generateSystem();
+	console.log(rhit.caveSystemGenerator.generateSystem());
 };
 
 rhit.main();

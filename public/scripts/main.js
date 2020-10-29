bdf44a6a73ee858a7e6dd226b823dbcd55c86edc
Griffin Annis <annisg@rose-hulman.edu>
2020-10-29 12:19:12 -0400

    generator stub, random num

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -9,9 +9,22 @@
 var rhit = rhit || {};
 
 rhit.navBarTemplate;
+rhit.caveSystemGenerator;
+
+rhit.CaveSystemGenerator = class {
+	constructor() {};
+
+	generateSystem(numCaves, generateEntranceExit = true) {
+		let cavesToGenerate = numCaves ? numCaves : Math.floor(Math.random() * 7) + 2;
+		console.log(cavesToGenerate);
+	};
+}
+
 
 rhit.main = function () {
 	$("#navBar").load("/templates.html #navBar");
+	rhit.caveSystemGenerator = new rhit.CaveSystemGenerator();
+	rhit.caveSystemGenerator.generateSystem();
 };
 
 rhit.main();

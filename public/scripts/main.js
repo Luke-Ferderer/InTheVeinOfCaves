8da4d80488b0416ee36b35da9523822618fb6256
Luke Ferderer <ferderl1@rose-hulman.edu>
2020-11-01 17:03:31 -0500

    Templating for browse

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -8,10 +8,17 @@
 
 var rhit = rhit || {};
 
-rhit.navBarTemplate;
-
 rhit.main = function () {
 	$("#navBar").load("/templates.html #navBar");
+	$("#browseMaps").load("/templates.html #browseMaps", () => {
+		$("#map0").load("/templates.html .map-item", () => {
+			$("#map1").load("/templates.html .map-item", () => {
+				$("#map2").load("/templates.html .map-item", () => {
+					$("#map3").load("/templates.html .map-item");
+				});
+			});
+		});
+	});
 };
 
 rhit.main();

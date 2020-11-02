d7cefd333b5ab4e47c8467802d7fef81b20fecc5
Luke Ferderer <ferderl1@rose-hulman.edu>
2020-11-02 03:19:09 -0500

    Slight loading changes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -91,8 +91,8 @@ rhit.randomRange = function(min, max) {
 }
 
 rhit.main = function () {
-	$("#navBar").load("/templates.html #navBar");
-	$("#browseMaps").load("/templates.html #browseMaps", () => {
+	$("#navBar").load("/templates.html #navBar > *");
+	$("#browseMaps").load("/templates.html #browseMaps > *", () => {
 		$("#map0").load("/templates.html .map-item", () => {
 			$("#map1").load("/templates.html .map-item", () => {
 				$("#map2").load("/templates.html .map-item", () => {
